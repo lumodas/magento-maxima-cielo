b060fd0dc8d846f774813517a225b2f2ae9530df
lawsann <lawsann@gmail.com>
2012-10-06 12:19:49 +0000

    Inclusao de restricao acerca de letras e numeros nos campos de numero do cartao e vodigo de seguranca. Inclusao dos dizeres da licensa GPL nos arquivos do modulo.

--- app/code/community/Maxima/Cielo/controllers/AdminController.php
+++ app/code/community/Maxima/Cielo/controllers/AdminController.php
@@ -1,154 +1,179 @@
 <?php
-	class Maxima_Cielo_AdminController extends Mage_Adminhtml_Controller_Action
+
+/*
+ * Maxima Cielo Module - payment method module for Magento, integrating
+ * the billing forms with a Cielo's gateway Web Service.
+ * Copyright (C) 2012  Fillipe Almeida Dutra
+ * Belo Horizonte, Minas Gerais - Brazil
+ * 
+ * Contact: lawsann@gmail.com
+ * Project link: http://code.google.com/p/magento-maxima-cielo/
+ * Group discussion: http://groups.google.com/group/cielo-magento
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class Maxima_Cielo_AdminController extends Mage_Adminhtml_Controller_Action
+{
+	/**
+	 * 
+	 * Funcao responsavel por consultar o status de uma transacao no WebService da 
+	 * Cielo
+	 * 
+	 */
+
+	public function consultAction()
 	{
-		/**
-		 * 
-		 * Funcao responsavel por consultar o status de uma transacao no WebService da 
-		 * Cielo
-		 * 
-		 */
-	
-		public function consultAction()
+		// verifica se o usuario estah logado na administracao do magento
+		Mage::getSingleton('core/session', array('name' => 'adminhtml'));
+		$session = Mage::getSingleton('admin/session');
+		
+		if (!$session->isLoggedIn())
 		{
-			// verifica se o usuario estah logado na administracao do magento
-			Mage::getSingleton('core/session', array('name' => 'adminhtml'));
-			$session = Mage::getSingleton('admin/session');
-			
-			if (!$session->isLoggedIn())
-			{
-				return;
-			}
-			
-			// pega pedido correspondente
-			$orderId = $this->getRequest()->getParam('order');
-			$order = Mage::getModel('sales/order')->load($orderId);
-			
-			// pega os dados para requisicao e realiza a consulta
-			$methodCode = $order->getPayment()->getMethodInstance()->getCode();
-			$cieloNumber 		= Mage::getStoreConfig('payment/' . $methodCode . '/cielo_number');
-			$cieloKey 			= Mage::getStoreConfig('payment/' . $methodCode . '/cielo_key');
-			$environment		= Mage::getStoreConfig('payment/' . $methodCode . '/environment');
-			$sslFile			= Mage::getStoreConfig('payment/' . $methodCode . '/ssl_file');
-			
-			$model = Mage::getModel('Maxima_Cielo/webServiceOrder', array('enderecoBase' => $environment, 'caminhoCertificado' => $sslFile));
-			
-			$model->tid = $this->getRequest()->getParam('tid');
-			$model->cieloNumber = $cieloNumber;
-			$model->cieloKey = $cieloKey;
-			
-			$model->requestConsultation();
-			$xml = $model->getXmlResponse();
-			
-			$this->getResponse()->setBody(Mage::helper('Maxima_Cielo')->xmlToHtml($xml));
+			return;
 		}
 		
+		// pega pedido correspondente
+		$orderId = $this->getRequest()->getParam('order');
+		$order = Mage::getModel('sales/order')->load($orderId);
 		
-		/**
-		 * 
-		 * Funcao responsavel por enviar o pedido de captura para o WebService da Cielo
-		 * 
-		 */
+		// pega os dados para requisicao e realiza a consulta
+		$methodCode = $order->getPayment()->getMethodInstance()->getCode();
+		$cieloNumber 		= Mage::getStoreConfig('payment/' . $methodCode . '/cielo_number');
+		$cieloKey 			= Mage::getStoreConfig('payment/' . $methodCode . '/cielo_key');
+		$environment		= Mage::getStoreConfig('payment/' . $methodCode . '/environment');
+		$sslFile			= Mage::getStoreConfig('payment/' . $methodCode . '/ssl_file');
+		
+		$model = Mage::getModel('Maxima_Cielo/webServiceOrder', array('enderecoBase' => $environment, 'caminhoCertificado' => $sslFile));
+		
+		$model->tid = $this->getRequest()->getParam('tid');
+		$model->cieloNumber = $cieloNumber;
+		$model->cieloKey = $cieloKey;
+		
+		$model->requestConsultation();
+		$xml = $model->getXmlResponse();
+		
+		$this->getResponse()->setBody(Mage::helper('Maxima_Cielo')->xmlToHtml($xml));
+	}
 	
-		public function captureAction()
+	
+	/**
+	 * 
+	 * Funcao responsavel por enviar o pedido de captura para o WebService da Cielo
+	 * 
+	 */
+
+	public function captureAction()
+	{
+		// verifica se o usuario estah logado na administracao do magento
+		Mage::getSingleton('core/session', array('name' => 'adminhtml'));
+		$session = Mage::getSingleton('admin/session');
+		
+		if (!$session->isLoggedIn())
 		{
-			// verifica se o usuario estah logado na administracao do magento
-			Mage::getSingleton('core/session', array('name' => 'adminhtml'));
-			$session = Mage::getSingleton('admin/session');
+			return;
+		}
+		
+		// pega pedido correspondente
+		$orderId = $this->getRequest()->getParam('order');
+		$order = Mage::getModel('sales/order')->load($orderId);
+		$value = Mage::helper('Maxima_Cielo')->formatValueForCielo($order->getGrandTotal());
+		
+		// pega os dados para requisicao e realiza a consulta
+		$cieloNumber 		= Mage::getStoreConfig('payment/Maxima_Cielo_Cc/cielo_number');
+		$cieloKey 			= Mage::getStoreConfig('payment/Maxima_Cielo_Cc/cielo_key');
+		$environment		= Mage::getStoreConfig('payment/' . $methodCode . '/environment');
+		$sslFile			= Mage::getStoreConfig('payment/' . $methodCode . '/ssl_file');
+		
+		$model = Mage::getModel('Maxima_Cielo/webServiceOrder', array('enderecoBase' => $environment, 'caminhoCertificado' => $sslFile));
+		
+		$model->tid = $this->getRequest()->getParam('tid');
+		$model->cieloNumber = $cieloNumber;
+		$model->cieloKey = $cieloKey;
+		
+		// requisita captura
+		$model->requestCapture($value);
+		$xml = $model->getXmlResponse();
+		$status = (string) $xml->status;
+		
+		// tudo ok, transacao aprovada, cria fatura
+		if($status == 6)
+		{
+			$html = "<b>Pedido capturado com sucesso!</b> &nbsp; &nbsp; 
+					<button type=\"button\" title=\"Atualizar Informações\" onclick=\"document.location.reload(true)\">
+						<span>Recarregar Página</span>
+					</button><br /><br />";
 			
-			if (!$session->isLoggedIn())
+			// atualiza os dados da compra
+			$payment = $order->getPayment();
+			$payment->setAdditionalInformation('Cielo_status', $status);
+			$payment->save();
+			
+			if($order->canInvoice() && !$order->hasInvoices())
 			{
-				return;
-			}
-			
-			// pega pedido correspondente
-			$orderId = $this->getRequest()->getParam('order');
-			$order = Mage::getModel('sales/order')->load($orderId);
-			$value = Mage::helper('Maxima_Cielo')->formatValueForCielo($order->getGrandTotal());
-			
-			// pega os dados para requisicao e realiza a consulta
-			$cieloNumber 		= Mage::getStoreConfig('payment/Maxima_Cielo_Cc/cielo_number');
-			$cieloKey 			= Mage::getStoreConfig('payment/Maxima_Cielo_Cc/cielo_key');
-			$environment		= Mage::getStoreConfig('payment/' . $methodCode . '/environment');
-			$sslFile			= Mage::getStoreConfig('payment/' . $methodCode . '/ssl_file');
-			
-			$model = Mage::getModel('Maxima_Cielo/webServiceOrder', array('enderecoBase' => $environment, 'caminhoCertificado' => $sslFile));
-			
-			$model->tid = $this->getRequest()->getParam('tid');
-			$model->cieloNumber = $cieloNumber;
-			$model->cieloKey = $cieloKey;
-			
-			// requisita captura
-			$model->requestCapture($value);
-			$xml = $model->getXmlResponse();
-			$status = (string) $xml->status;
-			
-			// tudo ok, transacao aprovada, cria fatura
-			if($status == 6)
-			{
-				$html = "<b>Pedido capturado com sucesso!</b> &nbsp; &nbsp; 
-						<button type=\"button\" title=\"Atualizar Informações\" onclick=\"document.location.reload(true)\">
-							<span>Recarregar Página</span>
-						</button><br /><br />";
+				$invoiceId = Mage::getModel('sales/order_invoice_api')->create($order->getIncrementId(), array());
+				$invoice = Mage::getModel('sales/order_invoice')->loadByIncrementId($invoiceId);
 				
-				// atualiza os dados da compra
-				$payment = $order->getPayment();
-				$payment->setAdditionalInformation('Cielo_status', $status);
-				$payment->save();
-				
-				if($order->canInvoice() && !$order->hasInvoices())
-				{
-					$invoiceId = Mage::getModel('sales/order_invoice_api')->create($order->getIncrementId(), array());
-					$invoice = Mage::getModel('sales/order_invoice')->loadByIncrementId($invoiceId);
-					
-					// envia email de confirmacao de fatura
-					$invoice->sendEmail(true);
-					$invoice->setEmailSent(true);
-					$invoice->save();
-				}
+				// envia email de confirmacao de fatura
+				$invoice->sendEmail(true);
+				$invoice->setEmailSent(true);
+				$invoice->save();
 			}
-			else
-			{
-				$html = "";
-			}
-			
-			$this->getResponse()->setBody($html . Mage::helper('Maxima_Cielo')->xmlToHtml($xml));
 		}
-		
-		
-		/**
-		 * 
-		 * Funcao responsavel por enviar o pedido de cancelamento para o WebService da Cielo
-		 * 
-		 */
-	
-		public function cancelAction()
+		else
 		{
-			// verifica se o usuario estah logado na administracao do magento
-			Mage::getSingleton('core/session', array('name' => 'adminhtml'));
-			$session = Mage::getSingleton('admin/session');
-			
-			if (!$session->isLoggedIn())
-			{
-				return;
-			}
-			
-			
-			// pega os dados para requisicao e realiza a consulta
-			$cieloNumber 		= Mage::getStoreConfig('payment/Maxima_Cielo_Cc/cielo_number');
-			$cieloKey 			= Mage::getStoreConfig('payment/Maxima_Cielo_Cc/cielo_key');
-			$environment		= Mage::getStoreConfig('payment/' . $methodCode . '/environment');
-			$sslFile			= Mage::getStoreConfig('payment/' . $methodCode . '/ssl_file');
-			
-			$model = Mage::getModel('Maxima_Cielo/webServiceOrder', array('enderecoBase' => $environment, 'caminhoCertificado' => $sslFile));
-			
-			$model->tid = $this->getRequest()->getParam('tid');
-			$model->cieloNumber = $cieloNumber;
-			$model->cieloKey = $cieloKey;
-			
-			// requisita cancelamento
-			$model->requestCancellation();
-			$xml = $model->getXmlResponse();
-			
-			$this->getResponse()->setBody(Mage::helper('Maxima_Cielo')->xmlToHtml($xml));
+			$html = "";
 		}
-	} 
+		
+		$this->getResponse()->setBody($html . Mage::helper('Maxima_Cielo')->xmlToHtml($xml));
+	}
+	
+	
+	/**
+	 * 
+	 * Funcao responsavel por enviar o pedido de cancelamento para o WebService da Cielo
+	 * 
+	 */
+
+	public function cancelAction()
+	{
+		// verifica se o usuario estah logado na administracao do magento
+		Mage::getSingleton('core/session', array('name' => 'adminhtml'));
+		$session = Mage::getSingleton('admin/session');
+		
+		if (!$session->isLoggedIn())
+		{
+			return;
+		}
+		
+		
+		// pega os dados para requisicao e realiza a consulta
+		$cieloNumber 		= Mage::getStoreConfig('payment/Maxima_Cielo_Cc/cielo_number');
+		$cieloKey 			= Mage::getStoreConfig('payment/Maxima_Cielo_Cc/cielo_key');
+		$environment		= Mage::getStoreConfig('payment/' . $methodCode . '/environment');
+		$sslFile			= Mage::getStoreConfig('payment/' . $methodCode . '/ssl_file');
+		
+		$model = Mage::getModel('Maxima_Cielo/webServiceOrder', array('enderecoBase' => $environment, 'caminhoCertificado' => $sslFile));
+		
+		$model->tid = $this->getRequest()->getParam('tid');
+		$model->cieloNumber = $cieloNumber;
+		$model->cieloKey = $cieloKey;
+		
+		// requisita cancelamento
+		$model->requestCancellation();
+		$xml = $model->getXmlResponse();
+		
+		$this->getResponse()->setBody(Mage::helper('Maxima_Cielo')->xmlToHtml($xml));
+	}
+} 
--- app/code/community/Maxima/Cielo/sql/Maxima_Cielo_setup/mysql4-upgrade-1.0.1-1.1.0.php
+++ app/code/community/Maxima/Cielo/sql/Maxima_Cielo_setup/mysql4-upgrade-1.0.1-1.1.0.php
@@ -1,60 +1,84 @@
 <?php
 
-	
-	$installer = Mage::getResourceModel('sales/setup', 'default_setup');
-	
-	$installer->startSetup();
-	
-	
-	// criacao dos campos de juros para as entidades envolvidas na compra
-	
-	$installer->addAttribute('order', 'base_interest', array
-	(
-		'label' => 'Base Interest',
-		'type'  => 'decimal',
-	));
-	
-	$installer->addAttribute('quote', 'interest', array
-	(
-		'label' => 'Interest',
-		'type'  => 'decimal',
-	));
+/*
+ * Maxima Cielo Module - payment method module for Magento, integrating
+ * the billing forms with a Cielo's gateway Web Service.
+ * Copyright (C) 2012  Fillipe Almeida Dutra
+ * Belo Horizonte, Minas Gerais - Brazil
+ * 
+ * Contact: lawsann@gmail.com
+ * Project link: http://code.google.com/p/magento-maxima-cielo/
+ * Group discussion: http://groups.google.com/group/cielo-magento
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-	$installer->addAttribute('quote', 'base_interest', array
-	(
-		'label' => 'Base Interest',
-		'type'  => 'decimal',
-	));
 	
-	$installer->addAttribute('order', 'interest', array
-	(
-		'label' => 'Interest',
-		'type'  => 'decimal',
-	));
+$installer = Mage::getResourceModel('sales/setup', 'default_setup');
 
-	$installer->addAttribute('invoice', 'base_interest', array
-	(
-		'label' => 'Base Interest',
-		'type'  => 'decimal',
-	));
+$installer->startSetup();
 
-	$installer->addAttribute('invoice', 'interest', array
-	(
-		'label' => 'Interest',
-		'type'  => 'decimal',
-	));
 
-	$installer->addAttribute('creditmemo', 'base_interest', array
-	(
-		'label' => 'Base Interest',
-		'type'  => 'decimal',
-	));
+// criacao dos campos de juros para as entidades envolvidas na compra
 
-	$installer->addAttribute('creditmemo', 'interest', array
-	(
-		'label' => 'Interest',
-		'type'  => 'decimal',
-	));
+$installer->addAttribute('order', 'base_interest', array
+(
+	'label' => 'Base Interest',
+	'type'  => 'decimal',
+));
 
-	$installer->endSetup();
-	
+$installer->addAttribute('quote', 'interest', array
+(
+	'label' => 'Interest',
+	'type'  => 'decimal',
+));
+
+$installer->addAttribute('quote', 'base_interest', array
+(
+	'label' => 'Base Interest',
+	'type'  => 'decimal',
+));
+
+$installer->addAttribute('order', 'interest', array
+(
+	'label' => 'Interest',
+	'type'  => 'decimal',
+));
+
+$installer->addAttribute('invoice', 'base_interest', array
+(
+	'label' => 'Base Interest',
+	'type'  => 'decimal',
+));
+
+$installer->addAttribute('invoice', 'interest', array
+(
+	'label' => 'Interest',
+	'type'  => 'decimal',
+));
+
+$installer->addAttribute('creditmemo', 'base_interest', array
+(
+	'label' => 'Base Interest',
+	'type'  => 'decimal',
+));
+
+$installer->addAttribute('creditmemo', 'interest', array
+(
+	'label' => 'Interest',
+	'type'  => 'decimal',
+));
+
+$installer->endSetup();
+
